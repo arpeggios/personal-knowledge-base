66f3ef97bede842e3489f10f895fbf4f15b85511
JM <jvcmoore@gmail.com>
2024-11-24 15:08:34 -0500

    added pitfalls section for useState page; added heading styles to theme

--- src/pages/UseStatePage.jsx
+++ src/pages/UseStatePage.jsx
@@ -1,11 +1,10 @@
-import { Typography, Link } from "@mui/material"
+import { Typography, Link, List, ListItem, Box } from '@mui/material'
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
 import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript'
-// import { monokai } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { monokai } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-import {useState} from 'react'
+import { useState } from 'react'
 
-const codeSample = `export function UseStatePage() {
+const codeExample = `export function UseStatePage() {
   const [count, setCount] = useState(0)
 
   function handleClick() {
@@ -19,6 +18,27 @@ const codeSample = `export function UseStatePage() {
   )
 }`
 
+const codeDoesNotWork1 = `// Definition with no return value
+function handleClick() {
+  setCount((count) => {count + 1})
+}`
+
+
+const codeWorks1 = `function handleClick() {
+  setCount((count) => {return count + 1})
+}`
+
+const codeWorks2 = `function handleClick() {
+  // Arrow functions that contain a simple expression and no curly braces around the body implicitly return the evaluated expression
+  setCount((count) => count + 1)
+}`
+
+const codeWorks3 = `function handleClick() {
+  // The "count" argument is removed here. It is in the other examples beause the example page that comes with every Vite project uses it.
+  setCount(() => count + 1)
+}
+`
+
 SyntaxHighlighter.registerLanguage('javascript', js)
 
 export function UseStatePage() {
@@ -30,12 +50,30 @@ export function UseStatePage() {
 
   return (
     <>
-      <Typography variant="h1">useState()</Typography>
-      <Typography><Link href="https://react.dev/reference/react/useState" target='_blank'>Official doc</Link></Typography>
+      <Typography variant='h1'>useState()</Typography>
+      <Typography><Link href='https://react.dev/reference/react/useState' target='_blank'>Official doc</Link></Typography>
       <Typography>The most common React Hook. State is data that can change. When state changes, the component and its children reload.</Typography>
-      <Typography variant="h2">Example</Typography>
-      <button onClick={handleClick}>Clicked {count} times</button>
-      <SyntaxHighlighter language="javascript" style={monokai} wrapLongLines>{codeSample}</SyntaxHighlighter>
+      <Box component='section'>
+        <Typography variant='h2'>Example</Typography>
+        <button onClick={handleClick}>Clicked {count} times</button>
+        <SyntaxHighlighter language='javascript' style={monokai} wrapLongLines>{codeExample}</SyntaxHighlighter>
+      </Box>
+      <Box component='section'>
+        <Typography variant='h2'>Pitfalls</Typography>
+        <List sx={{ listStyleType: 'disc', pl: 2 }}>
+          <ListItem sx={{ display: 'list-item' }}>
+            <Typography>
+              setCount is a function that accepts a value or something that returns a value (like an expression or a function that returns a value). You can't put a function definition in there that doesn't return a value.
+            </Typography>
+            <Typography variant='h3'>Doesn't work</Typography>
+            <SyntaxHighlighter language='javascript' style={monokai}>{codeDoesNotWork1}</SyntaxHighlighter>
+            <Typography variant='h3'>Works</Typography>
+            <SyntaxHighlighter language='javascript' style={monokai} wrapLongLines>{codeWorks1}</SyntaxHighlighter>
+            <SyntaxHighlighter language='javascript' style={monokai} wrapLongLines>{codeWorks2}</SyntaxHighlighter>
+            <SyntaxHighlighter language='javascript' style={monokai} wrapLongLines>{codeWorks3}</SyntaxHighlighter>
+          </ListItem>
+        </List>
+      </Box>
     </>
   )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,39 @@
 import { createTheme } from "@mui/material";
 
+const headingFontWeight = 300,
+headingMargin = '1rem 0'
+
 export const theme = createTheme({
   typography: {
     h1: {
       fontSize: "3rem",
-      margin: "1rem auto"
+      margin: headingMargin,
+      fontWeight: headingFontWeight,
     },
     h2: {
       fontSize: "2rem",
-      margin: "1rem 0"
+      margin: headingMargin,
+      fontWeight: headingFontWeight,
+    },
+    h3: {
+      fontSize: "1.8rem",
+      margin: headingMargin,
+      fontWeight: headingFontWeight,
+    },
+    h4: {
+      fontSize: "1.6rem",
+      margin: headingMargin,
+      fontWeight: headingFontWeight,
+    },
+    h5: {
+      fontSize: "1.4rem",
+      margin: headingMargin,
+      fontWeight: headingFontWeight,
+    },
+    h6: {
+      fontSize: "1.2rem",
+      margin: headingMargin,
+      fontWeight: headingFontWeight,
     }
   }
 })
